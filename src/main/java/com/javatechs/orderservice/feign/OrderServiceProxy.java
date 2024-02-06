2d0c3758564f47cb5d9a15ec13e6fcae874167c6
KapilSable07 <kapilsable07@gmail.com>
2024-02-06 23:29:18 +0530

    Feign client example with and without URL

--- src/main/java/com/javatechs/orderservice/feign/OrderServiceProxy.java
+++ src/main/java/com/javatechs/orderservice/feign/OrderServiceProxy.java
@@ -5,10 +5,14 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "payment-service",url = "http://localhost:9191/payment/" )
+// With URL specified client will find service with given URL
+//@FeignClient(name = "PAYMENT-SERVICE",url = "http://localhost:9191/" )
+
+// Without URL : Client will finde service by name in service registory
+@FeignClient(name = "PAYMENT-SERVICE" )
 public interface OrderServiceProxy {
 
-    @GetMapping("/getPayment/{paymentId}")
+    @GetMapping("/payment/getPayment/{paymentId}")
     public Payment getPayment(@PathVariable("paymentId") int paymentId);
 
 }
